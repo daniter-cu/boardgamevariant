1e35e950df8d1d506887d6167e9e31474c92650d
iter.dan@gmail.com <iter.dan@gmail.com@16114d1b-b542-34ae-8e9f-2f1a378246cf>
2010-09-17 16:09:34 +0000

    Added bidbuilder

---
+++ revised/src/seven/f10/g6/BidBuilder.java
@@ -0,0 +1,28 @@
+package seven.f10.g6;
+
+public class BidBuilder {
+
+	/**
+	 * Create BidBuilder object
+	 */
+	public BidBuilder()
+	{
+		//no-op
+	}
+	
+	public int bid()
+	{
+		return 0;
+	}
+	
+	public int distance()
+	{
+		return 0;
+	}
+	
+	public int make7()
+	{
+		return 0;
+	}
+	
+}
